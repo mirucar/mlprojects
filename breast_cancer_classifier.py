7afd4045869107f85032542f741970f1f81862e9
mirucar <63228093+mirucar@users.noreply.github.com>
2021-04-30 03:03:44 +0300

    Machine Learning Models #2
    Breast Cancer Classifier Model
    K-Nearest Neighbor

---
+++ breast_cancer_classifier.py
@@ -0,0 +1,50 @@
+import codecademylib3_seaborn
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+
+breast_cancer_data = load_breast_cancer()
+
+print(breast_cancer_data.data[0])
+print(breast_cancer_data.feature_names)
+
+print(breast_cancer_data.target)
+print(breast_cancer_data.target_names)
+
+training_data ,validation_data ,training_labels ,validation_labels = train_test_split(
+  breast_cancer_data.data, 
+  breast_cancer_data.target, 
+  test_size = 0.2, 
+  random_state = 100
+  )
+
+print(len(training_data))
+print(len(training_labels))
+
+accuracies = []
+for k in range(1,101):
+  classifier = KNeighborsClassifier(n_neighbors = k)
+  classifier.fit(training_data, training_labels)
+  validation_accuracy = classifier.score(validation_data,validation_labels)
+  accuracies.append(validation_accuracy)
+print(validation_accuracy)
+
+k_list = range(1, 101)
+
+plt.plot(k_list, accuracies)
+plt.xlabel("k")
+plt.ylabel("Validation Accuracy")
+plt.title("Breast Cancer Classifier Accuracy")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
